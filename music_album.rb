1f54fa3f6c03b06b39caaac9610472536490734e
mike2611 <mpuentes2611@gmail.com>
2022-01-04 10:42:10 -0600

    Fix linter erros

--- music_album.rb
+++ music_album.rb
@@ -1,8 +1,8 @@
 require_relative './item'
 
-class MusicAlbum < Item 
+class MusicAlbum < Item
   attr_accessor :on_spotify
-  
+
   def initialize(publish_date, archived, on_spotify, id = Random.rand(1..1000))
     super(publish_date, archived, id)
     @on_spotify = on_spotify
